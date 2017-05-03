240d86fe041420e2438ac4fa22a27c0dd2906c93
Casa <Casa@Casa-PC>
2017-05-03 16:02:10 -0300

    Se agregaron dos clases mas para entender el como funciona el codigo que saque de google.

--- P3TP217/src/caminoMinimo/MainClass.java
+++ P3TP217/src/caminoMinimo/MainClass.java
@@ -12,8 +12,25 @@ public class MainClass {
 		System.out.println(ins.get_ciudades().get("asd"));
 		System.out.println(ins.get_ciudades().get("qwe"));
 		
-		Solver solver = new Solver(2);
-		solver.resuelve(0);
+//		Solver solver = new Solver(2);
+//		solver.resuelve(0);
+		
+		
+		  final WeightedGraph t = new WeightedGraph (3);
+		         t.setLabel (0, "v0");
+		         t.setLabel (1, "v1");
+		         t.setLabel (2, "v2");
+		         t.addEdge(0, 1, 1);
+		         t.addEdge(0, 2, 5);
+		         t.addEdge(1, 2, 3);
+		         t.addEdge(1, 0, 1);
+		         t.addEdge(2, 0, 5);
+		         t.print();
+		   
+		         final int [] pred = dijkstra.dijkstra (t, 0);
+		         for (int n=0; n<3; n++) {
+		            dijkstra.printPath (t, pred, 0, n);
+		         }
 		
 	}
 

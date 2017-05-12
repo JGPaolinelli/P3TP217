998853e43d084d0832d968ed31688e6e40847a68
Gonzalo <Gonzalo@JGPaolinelli>
2017-05-11 22:55:32 -0300

    Cambios en la vista

--- P3TP217/src/caminoMinimo/Main.java
+++ P3TP217/src/caminoMinimo/Main.java
@@ -22,6 +22,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.border.EtchedBorder;
 
 public class Main {
 
@@ -68,78 +74,44 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JMapViewer jMapViewer = new JMapViewer();
-		jMapViewer.setBounds(10, 11, 964, 359);
-		frame.getContentPane().add(jMapViewer);
-		contid = 0;
-		contruta = 0;
-
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(430, 408, 255, 122);
-		frame.getContentPane().add(panel_1);
-		panel_1.setLayout(null);
-		panel_1.setVisible(false);
-
-		JPanel panel = new JPanel();
-		panel.setBounds(719, 381, 255, 169);
-		frame.getContentPane().add(panel);
-		panel.setLayout(null);
-		panel.setVisible(false);
-
 		List<Vertice> vertices = new ArrayList<Vertice>();
 		List<Arista> aristas = new ArrayList<Arista>();
 
-		JButton btnAgregarCiudad = new JButton("Agregar Ciudad");
-		btnAgregarCiudad.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				panel_1.setVisible(true);
-			}
-		});
-		btnAgregarCiudad.setBounds(86, 415, 136, 23);
-		frame.getContentPane().add(btnAgregarCiudad);
+		JMapViewer jMapViewer = new JMapViewer();
+		jMapViewer.setBounds(0, 0, 984, 540);
+		frame.getContentPane().add(jMapViewer);
 
-		JButton btnAgregarCamino = new JButton("Agregar Camino");
-		btnAgregarCamino.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (panel_1.isVisible()) {
-					panel_1.setVisible(false);
-					panel.setVisible(true);
-				} else {
-					panel.setVisible(true);
-				}
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(769, 369, 205, 160);
+		jMapViewer.add(panel_1);
+		panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_1.setLayout(null);
+		panel_1.setVisible(false);
 
-			}
-		});
-		btnAgregarCamino.setBounds(86, 460, 136, 23);
-		frame.getContentPane().add(btnAgregarCamino);
-
-		// btnAsd.setBounds(69, 283, 89, 23);
-		// frame.getContentPane().add(btnAsd);
-
-		JLabel lblNewLabel = new JLabel("Nombre de la ciudad:");
-		lblNewLabel.setBounds(10, 11, 121, 14);
+		JLabel lblNewLabel = new JLabel("Nombre:");
+		lblNewLabel.setBounds(10, 44, 76, 14);
 		panel_1.add(lblNewLabel);
 
 		JLabel lblLatitud = new JLabel("Latitud:");
-		lblLatitud.setBounds(10, 36, 111, 14);
+		lblLatitud.setBounds(10, 72, 76, 14);
 		panel_1.add(lblLatitud);
 
 		JLabel lblLongitud = new JLabel("Longitud:");
-		lblLongitud.setBounds(10, 61, 121, 14);
+		lblLongitud.setBounds(10, 100, 76, 14);
 		panel_1.add(lblLongitud);
 
 		textField_NombreCiudad = new JTextField();
-		textField_NombreCiudad.setBounds(131, 8, 111, 20);
+		textField_NombreCiudad.setBounds(69, 41, 111, 20);
 		panel_1.add(textField_NombreCiudad);
 		textField_NombreCiudad.setColumns(10);
 
 		textField_Latitud = new JTextField();
-		textField_Latitud.setBounds(131, 33, 111, 20);
+		textField_Latitud.setBounds(69, 69, 111, 20);
 		panel_1.add(textField_Latitud);
 		textField_Latitud.setColumns(10);
 
 		textField_Longitud = new JTextField();
-		textField_Longitud.setBounds(131, 58, 111, 20);
+		textField_Longitud.setBounds(69, 97, 111, 20);
 		panel_1.add(textField_Longitud);
 		textField_Longitud.setColumns(10);
 
@@ -150,9 +122,9 @@ public class Main {
 						&& !textField_Longitud.getText().equals("")) {
 					textField_Latitud.setText(textField_Latitud.getText().replace('.', ','));
 					textField_Longitud.setText(textField_Longitud.getText().replace('.', ','));
-					if (!isNumeric(textField_Latitud.getText())) {
+					if (!esNumero(textField_Latitud.getText())) {
 						alerta("No ingresaste una latitud correcta!", "Latitud invalido");
-					} else if (!isNumeric(textField_Longitud.getText())) {
+					} else if (!esNumero(textField_Longitud.getText())) {
 						alerta("No ingresaste una longitud correcta!", "Latitud invalido");
 					} else {
 						vertices.add(new Vertice(Integer.toString(contid), textField_NombreCiudad.getText(),
@@ -180,90 +152,74 @@ public class Main {
 
 			}
 		});
-		btnAceptarCiudad.setBounds(91, 89, 89, 23);
+		btnAceptarCiudad.setBounds(109, 128, 86, 24);
 		panel_1.add(btnAceptarCiudad);
+		
+		JLabel lblVertice = new JLabel("Vertice");
+		lblVertice.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+		lblVertice.setBounds(69, 0, 62, 30);
+		panel_1.add(lblVertice);
+		
+		JButton btnCancelarCiudad = new JButton("Cancelar");
+		btnCancelarCiudad.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel_1.setVisible(false);
+			}
+		});
+		btnCancelarCiudad.setBounds(10, 128, 86, 24);
+		panel_1.add(btnCancelarCiudad);
 
-		JLabel lblNombreCamino = new JLabel("Nombre del camino:");
-		lblNombreCamino.setBounds(10, 11, 122, 14);
+		JPanel panel = new JPanel();
+		panel.setBounds(769, 329, 205, 200);
+		jMapViewer.add(panel);
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel.setLayout(null);
+		panel.setVisible(false);
+
+		JLabel lblNombreCamino = new JLabel("Nombre:");
+		lblNombreCamino.setBounds(10, 39, 61, 14);
 		panel.add(lblNombreCamino);
 
-		JLabel lblInicio = new JLabel("Inicio: ");
-		lblInicio.setBounds(10, 36, 111, 14);
+		JLabel lblInicio = new JLabel("Desde: ");
+		lblInicio.setBounds(10, 64, 61, 14);
 		panel.add(lblInicio);
 
-		JLabel lblFin = new JLabel("Fin: ");
-		lblFin.setBounds(10, 61, 46, 14);
+		JLabel lblFin = new JLabel("Hasta: ");
+		lblFin.setBounds(10, 89, 61, 14);
 		panel.add(lblFin);
 
 		JLabel lblDistancia = new JLabel("Distancia: ");
-		lblDistancia.setBounds(10, 89, 111, 14);
+		lblDistancia.setBounds(10, 114, 61, 14);
 		panel.add(lblDistancia);
 
-		JLabel lblPeaje = new JLabel("Tiene peaje?");
-		lblPeaje.setBounds(10, 114, 111, 14);
+		JLabel lblPeaje = new JLabel("Peaje:");
+		lblPeaje.setBounds(10, 139, 61, 14);
 		panel.add(lblPeaje);
 
 		textField_NombreCamino = new JTextField();
 		textField_NombreCamino.setColumns(10);
-		textField_NombreCamino.setBounds(131, 8, 111, 20);
+		textField_NombreCamino.setBounds(72, 36, 111, 20);
 		panel.add(textField_NombreCamino);
 
 		textField_Inicio = new JTextField();
 		textField_Inicio.setColumns(10);
-		textField_Inicio.setBounds(131, 33, 111, 20);
+		textField_Inicio.setBounds(72, 61, 111, 20);
 		panel.add(textField_Inicio);
 
 		textField_Fin = new JTextField();
 		textField_Fin.setColumns(10);
-		textField_Fin.setBounds(131, 58, 111, 20);
+		textField_Fin.setBounds(72, 86, 111, 20);
 		panel.add(textField_Fin);
 
 		textField_Distancia = new JTextField();
 		textField_Distancia.setColumns(10);
-		textField_Distancia.setBounds(131, 83, 111, 20);
+		textField_Distancia.setBounds(72, 111, 111, 20);
 		panel.add(textField_Distancia);
 
 		textField_Peaje = new JTextField();
 		textField_Peaje.setColumns(10);
-		textField_Peaje.setBounds(131, 108, 111, 20);
+		textField_Peaje.setBounds(72, 136, 111, 20);
 		panel.add(textField_Peaje);
-
-		JButton btnCalcular = new JButton("Calcular");
-		btnCalcular.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Grafo g = new Grafo(vertices, aristas);
-				LinkedList<Vertice> camino = null;
-				String cantidadPeajes = JOptionPane.showInputDialog(frame, "Ingresa la cantidad de peajes:");
-				String ciudadInicial = JOptionPane.showInputDialog(frame, "Ingresa desde que ciudad queres partir:");
-				String ciudadFinal = JOptionPane.showInputDialog(frame, "Ingresa a que ciudad queres llegar:");
-				Dijkstra d = new Dijkstra(g, new Integer(cantidadPeajes));
-				for (Vertice vertice : vertices) {
-					if (vertice.getNombre().equals(ciudadInicial)) {
-						d.ejecutar(vertices.get(new Integer(vertice.getId())));
-					}
-					if (vertice.getNombre().equals(ciudadFinal)) {
-						camino = d.getCamino(vertices.get(new Integer(vertice.getId())));
-					}
-				}
-
-				for (int i = 0; i < camino.size(); i++) {
-					if (i + 1 < camino.size()) {
-						Coordinate one = new Coordinate(camino.get(i).getLongitud(), camino.get(i).getLatitud());
-						Coordinate two = new Coordinate(camino.get(i + 1).getLongitud(),
-								camino.get(i + 1).getLatitud());
-						List<Coordinate> route = new ArrayList<Coordinate>(Arrays.asList(one, two, two));
-						MapPolygonImpl r = new MapPolygonImpl(route);
-						r.setColor(Color.GREEN);
-						jMapViewer.addMapPolygon(r);
-					} else {
-						break;
-					}
-				}
-			}
-		});
-		btnCalcular.setBounds(109, 507, 91, 23);
-		frame.getContentPane().add(btnCalcular);
-		btnCalcular.setVisible(false);
 
 		JButton btnAceptarCamino = new JButton("Aceptar");
 		btnAceptarCamino.addActionListener(new ActionListener() {
@@ -308,7 +264,7 @@ public class Main {
 						if (verticeAux_1.equals(verticeAux_2)) {
 							alerta("No se pueden agregar bucles!", "No se creo el camino");
 							hayError = true;
-						} else if (!isNumeric(textField_Distancia.getText())) {
+						} else if (!esNumero(textField_Distancia.getText())) {
 							alerta("No ingresaste una distancia correcta!", "Distancia invalido");
 							hayError = true;
 						} else {
@@ -343,15 +299,108 @@ public class Main {
 						peaje = false;
 						jMapViewer.addMapPolygon(r);
 						contruta++;
-						if (aristas.size() >= 2) {
-							btnCalcular.setVisible(true);
-						}
 					}
 				}
 			}
 		});
-		btnAceptarCamino.setBounds(90, 139, 89, 23);
+		btnAceptarCamino.setBounds(106, 164, 86, 24);
 		panel.add(btnAceptarCamino);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel.setVisible(false);
+			}
+		});
+		btnCancelar.setBounds(10, 164, 86, 24);
+		panel.add(btnCancelar);
+		
+		JLabel lblArista = new JLabel("Arista");
+		lblArista.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 15));
+		lblArista.setBounds(75, 11, 52, 14);
+		panel.add(lblArista);
+		contid = 0;
+		contruta = 0;
+
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+
+		JMenu mnMenu = new JMenu("Menu");
+		menuBar.add(mnMenu);
+
+		JMenu mnAgregar = new JMenu("Agregar");
+		mnAgregar.setIcon(new ImageIcon(Main.class.getResource("/org/openstreetmap/gui/jmapviewer/images/plus.png")));
+		mnMenu.add(mnAgregar);
+
+		JMenuItem mntmVertice = new JMenuItem("Vertice");
+		mntmVertice.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				if (panel.isVisible()) {
+					panel.setVisible(false);
+					panel_1.setVisible(true);
+				} else {
+					panel_1.setVisible(true);
+				}
+			}
+		});
+		mnAgregar.add(mntmVertice);
+
+		JMenuItem mntmArista = new JMenuItem("Arista");
+		mntmArista.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (panel_1.isVisible()) {
+					panel_1.setVisible(false);
+					panel.setVisible(true);
+				} else {
+					panel.setVisible(true);
+				}
+			}
+		});
+		mnAgregar.add(mntmArista);
+
+		JMenuItem mntmCalcularCaminoMinimo = new JMenuItem("Calcular Camino Minimo");
+		mntmCalcularCaminoMinimo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (aristas.size() >= 2) {
+					Grafo g = new Grafo(vertices, aristas);
+					LinkedList<Vertice> camino = null;
+					String cantidadPeajes = JOptionPane.showInputDialog(frame, "Ingresa la cantidad de peajes:");
+					String ciudadInicial = JOptionPane.showInputDialog(frame,
+							"Ingresa desde que ciudad queres partir:");
+					String ciudadFinal = JOptionPane.showInputDialog(frame, "Ingresa a que ciudad queres llegar:");
+					Dijkstra d = new Dijkstra(g, new Integer(cantidadPeajes));
+					for (Vertice vertice : vertices) {
+						if (vertice.getNombre().equals(ciudadInicial)) {
+							d.ejecutar(vertices.get(new Integer(vertice.getId())));
+						}
+						if (vertice.getNombre().equals(ciudadFinal)) {
+							camino = d.getCamino(vertices.get(new Integer(vertice.getId())));
+						}
+					}
+
+					for (int i = 0; i < camino.size(); i++) {
+						if (i + 1 < camino.size()) {
+							Coordinate one = new Coordinate(camino.get(i).getLongitud(), camino.get(i).getLatitud());
+							Coordinate two = new Coordinate(camino.get(i + 1).getLongitud(),
+									camino.get(i + 1).getLatitud());
+							List<Coordinate> route = new ArrayList<Coordinate>(Arrays.asList(one, two, two));
+							MapPolygonImpl r = new MapPolygonImpl(route);
+							r.setColor(Color.GREEN);
+							jMapViewer.addMapPolygon(r);
+						} else {
+							break;
+						}
+					}
+				} else {
+					alerta("No hay las suficientes aristas para calcular un camino!", "Incalculable");
+				}
+			}
+		});
+		mnMenu.add(mntmCalcularCaminoMinimo);
+
+		JMenuItem mntmCerrar = new JMenuItem("Cerrar");
+		mnMenu.add(mntmCerrar);
 
 		jMapViewer.setDisplayPositionByLatLon(-34.6083, -58.3712, 5);
 
@@ -361,7 +410,7 @@ public class Main {
 		JOptionPane.showMessageDialog(null, mensaje, "Error: " + titulo, JOptionPane.INFORMATION_MESSAGE);
 	}
 
-	private static boolean isNumeric(String cadena) {
+	private static boolean esNumero(String cadena) {
 		try {
 			Integer.parseInt(cadena);
 			return true;

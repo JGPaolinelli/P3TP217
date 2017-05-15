4a8b1a32e1d5da23e3a4a73d619078987688af3d
Gonzalo <Gonzalo@JGPaolinelli>
2017-05-14 22:42:17 -0300

    Mas cambios de nombres. Se agrega Test para dijkstra vacio.

--- P3TP217/src/caminoMinimo/Main.java
+++ P3TP217/src/caminoMinimo/Main.java
@@ -1,91 +1,49 @@
 package caminoMinimo;
 
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-
-import javax.swing.JFrame;
-
-import org.openstreetmap.gui.jmapviewer.JMapViewer;
 
 public class Main {
 
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Main window = new Main();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+		
+//		List<Vertice> v = new ArrayList<Vertice>();
+//		v.add(new Vertice("0", "Buenos Aires", 0.0, 3.0));
+//		v.add(new Vertice("1", "La Pampa", 1.0, 4.0));
+//		v.add(new Vertice("2", "2", 5.0, 17.0));
+//		v.add(new Vertice("3", "Tu vieja", 15.0, 18.0));
+//		v.add(new Vertice("4", "Cordoba", 15.0, 18.0));
+//		v.add(new Vertice("5", "Mendoza", 15.0, 18.0));
+//		v.add(new Vertice("6", "Salta", 15.0, 18.0));
+//		List<Arista> a = new ArrayList<Arista>();
+//		a.add(new Arista("0", v.get(0), v.get(1), 1, true));
+//		a.add(new Arista("1", v.get(0), v.get(2), 1, true));
+//		a.add(new Arista("2", v.get(1), v.get(3), 3, false));
+//		a.add(new Arista("4", v.get(1), v.get(4), 2, false));
+//		a.add(new Arista("5", v.get(2), v.get(4), 4, true));
+//		a.add(new Arista("6", v.get(3), v.get(5), 2, true));
+//		a.add(new Arista("7", v.get(4), v.get(6), 15, true));
+//		a.add(new Arista("8", v.get(5), v.get(6), 10, false));
+//		System.out.println(v.get(0).getNombre().equals(s));
+		
+		List<Vertice> v = new ArrayList<Vertice>();
+		List<Arista> a = new ArrayList<Arista>();
+		v.add(new Vertice("0", "Buenos Aires", 0.0, 3.0));
+		v.add(new Vertice("1", "La Pampa", 1.0, 4.0));
+		v.add(new Vertice("2", "asd", 5.0, 17.0));
+		a.add(new Arista("bsas pmp", v.get(0), v.get(1), 1, true));
+		a.add(new Arista("bsas asd", v.get(0), v.get(2), 5, true));
+		a.add(new Arista("pmp asd", v.get(1), v.get(2), 3, false));
+		
+		
+		Grafo g = new Grafo(v, a);
+		Dijkstra d = new Dijkstra(g,1);
+		d.ejecutar(g.getVertices().get(0));
+		LinkedList<Vertice> camino  = d.getCamino(v.get(2));
 
-	/**
-	 * Create the application.
-	 */
-	public Main() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1000, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		List<Vertice> vertices = new ArrayList<Vertice>();
-		List<Arista> aristas = new ArrayList<Arista>();
-
-		JMapViewer jMapViewer = new JMapViewer();
-		jMapViewer.setBounds(0, 0, 984, 540);
-		frame.getContentPane().add(jMapViewer);
-
-		FormularioCiudad formularioCiudad = new FormularioCiudad(vertices, jMapViewer);
-		jMapViewer.add(formularioCiudad.getjPanel_Ciudad());
-
-		FormularioCamino formularioCamino = new FormularioCamino(vertices, aristas, jMapViewer, frame);
-		jMapViewer.add(formularioCamino.getPanel());
-		jMapViewer.setDisplayPositionByLatLon(-34.6083, -58.3712, 5);
-
-		Menu menu = new Menu();
-		frame.setJMenuBar(menu.getMenuBar());
-
-		menu.getMntmVertice().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (formularioCamino.isVisible()) {
-					formularioCamino.setVisible(false);
-					formularioCiudad.setVisible(true);
-				} else {
-					formularioCiudad.setVisible(true);
-				}
-			}
-		});
-		menu.getMntmArista().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (formularioCiudad.isVisible()) {
-					formularioCiudad.setVisible(false);
-					formularioCamino.setVisible(true);
-				} else {
-					formularioCamino.setVisible(true);
-				}
-			}
-		});
-		menu.getMntmCalcularCaminoMinimo().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				formularioCamino.calcularCaminoMinimo(vertices, aristas, jMapViewer);
-			}
-		});
+		for (Vertice vertex : camino) {
+            System.out.println(vertex);
+    }	
 	}
 }

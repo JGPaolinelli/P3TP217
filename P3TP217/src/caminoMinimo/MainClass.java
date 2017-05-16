7765a9168294fa0165150f2f9fdda609a19e307c
crisaguayo <aguayo.cristiandavid@gmail.com>
2017-05-16 01:18:38 -0300

    Se borro el main viejo y se cambio el nombre de MainClass a Main y de Main a MainClass

--- P3TP217/src/caminoMinimo/MainClass.java
+++ P3TP217/src/caminoMinimo/MainClass.java
@@ -1,91 +1,29 @@
 package caminoMinimo;
 
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-
-import javax.swing.JFrame;
-
-import org.openstreetmap.gui.jmapviewer.JMapViewer;
 
 public class MainClass {
 
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainClass window = new MainClass();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the application.
-	 */
-	public MainClass() {
-		initialize();
-	}
+		List<Vertice> v = new ArrayList<Vertice>();
+		List<Arista> a = new ArrayList<Arista>();
+		v.add(new Vertice("0", "Buenos Aires", 0.0, 3.0));
+		v.add(new Vertice("1", "La Pampa", 1.0, 4.0));
+		v.add(new Vertice("2", "Cordoba", 5.0, 17.0));
+		v.add(new Vertice("3", "Santa Cruz", 4.0, 15.0));
+		a.add(new Arista("Buenos Aires La Pampa", v.get(0), v.get(1), 1, true));
+		a.add(new Arista("Buenos Aires Cordoba", v.get(0), v.get(2), 5, true));
+		a.add(new Arista("La Pampa Cordoba", v.get(1), v.get(2), 3, false));
+		a.add(new Arista("Buenos Aires Santa Cruz", v.get(0), v.get(3),1,false));
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1000, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		List<Vertice> vertices = new ArrayList<Vertice>();
-		List<Arista> aristas = new ArrayList<Arista>();
-
-		JMapViewer jMapViewer = new JMapViewer();
-		jMapViewer.setBounds(0, 0, 984, 540);
-		frame.getContentPane().add(jMapViewer);
-
-		FormularioCiudad formularioCiudad = new FormularioCiudad(vertices, jMapViewer);
-		jMapViewer.add(formularioCiudad.getjPanel_Ciudad());
-
-		FormularioCamino formularioCamino = new FormularioCamino(vertices, aristas, jMapViewer, frame);
-		jMapViewer.add(formularioCamino.getPanel());
-		jMapViewer.setDisplayPositionByLatLon(-34.6083, -58.3712, 5);
-
-		Menu menu = new Menu();
-		frame.setJMenuBar(menu.getMenuBar());
-
-		menu.getMntmVertice().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (formularioCamino.isVisible()) {
-					formularioCamino.setVisible(false);
-					formularioCiudad.setVisible(true);
-				} else {
-					formularioCiudad.setVisible(true);
-				}
-			}
-		});
-		menu.getMntmArista().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (formularioCiudad.isVisible()) {
-					formularioCiudad.setVisible(false);
-					formularioCamino.setVisible(true);
-				} else {
-					formularioCamino.setVisible(true);
-				}
-			}
-		});
-		menu.getMntmCalcularCaminoMinimo().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				formularioCamino.calcularCaminoMinimo(vertices, aristas, jMapViewer);
-			}
-		});
+		Grafo g = new Grafo(v, a);
+		Dijkstra d = new Dijkstra(g, 1);
+		d.ejecutar(g.getVertices().get(0));
+		LinkedList<Vertice> camino = d.getCamino(v.get(3));
+		
+		System.out.println(camino.toString());
 	}
 }
